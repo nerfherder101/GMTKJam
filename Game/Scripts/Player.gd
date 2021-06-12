51111682fea828930ec897579a1085ac877cffda
Nerfherder <haydsteed@gmail.com>
2021-06-11 21:42:04 -0700

    Began writing control changing

--- Game/Scripts/Player.gd
+++ Game/Scripts/Player.gd
@@ -1,26 +1,37 @@
 extends KinematicBody2D
 
-var G = 10
+var G = 20
 var speed = 300
-var jump_velocity = 400
+var jump_velocity = 500
 var input = Vector2()
 var velocity = Vector2()
+
+var controls_default = {
+	"ui_right" : Vector2 (1,0),
+	"ui_left" : Vector2 (-1,0),
+	"ui_up" : Vector2 (0,1)
+}
+
+var controls_binded = {
+	"ui_right" : "ui_right",
+	"ui_left" :  "ui_up",
+	"ui_up" : "ui_left"
+}
 
 func _ready():
 	pass 
 
 func get_input ():
-	if Input.is_action_pressed("ui_right"):
-		input.x = 1
-	elif Input.is_action_pressed("ui_left"):
-		input.x = -1
-	else: 
-		input.x = 0
+	input.x = 0
+	
+	var key_array = Array(controls_default.keys())
+	
+	for i in controls_default:
+		if (Input.is_action_pressed(i)):
+			input += controls_default[i]
+			input += controls_default[controls_binded[i]]
 		
-	if Input.is_action_pressed("ui_up") && is_on_floor():
-		input.y = 1
-
-	else: 
+	if !is_on_floor():
 		input.y = 0
 	pass
 
