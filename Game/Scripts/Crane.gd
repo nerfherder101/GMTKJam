4204c72da2a937fc9b5de0eb8fbc1226a5f8e25a
01271 <3951049+01271@users.noreply.github.com>
2021-06-12 00:55:43 -0700

    Created crane scene, crane animation, changed the default image import mode so it doesn't blur sprites, added a ton of images.

---
+++ Game/Scripts/Crane.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+
+#Draw a line from the top of the screen to where the crane is.
+
+func _draw():
+	var pos = get_position()
+	draw_line(Vector2(0,-1000), Vector2(0, 0), Color(0, 0, 0), 1)
+	print (pos)
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
